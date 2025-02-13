f1ba550b769e9c1d65e6c714734079036021419e
uladek.kish <vldislav.kish@gmail.com>
2025-02-13 21:27:33 +0300

    ex#1 hw#6.1

--- hw#6.1.py
+++ hw#6.1.py
@@ -8,9 +8,9 @@ def moto_time(inp_m):
     while inp_m >= 60:
         inp_m -= 60
         inp_h += 1
-    ans_m = list(map(int, [_ for _ in str(inp_m)]))
-    ans_h = list(map(int, [_ for _ in str(inp_h)]))
-    print(datetime.time(hour= inp_h, minute=inp_m), sum(ans_h + ans_m))
+    ans_m = list(map(int, list(str(inp_m))))
+    ans_h = list(map(int, list(str(inp_h))))
+    print(datetime.time(hour=inp_h, minute=inp_m), sum(ans_h + ans_m))
 
 
 moto_time(1439)
