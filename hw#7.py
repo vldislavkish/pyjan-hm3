30cdc3aeea3b8d66b3c5ee07bae4d17a8572483d
uladek.kish <vldislav.kish@gmail.com>
2025-02-19 16:19:32 +0300

    hw#7

--- hw#7.py
+++ hw#7.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 
 def bulls_and_cows():
@@ -7,22 +8,26 @@ def bulls_and_cows():
     while digits[0] == 0:
         random.shuffle(digits)
     hidden_number = digits[:4]
+    print('Загаданное число: ', end='')
     print(*hidden_number, sep='')
-    inp = int(input())
-    while inp != hidden_number:
+
+    while True:
+        inp = input('Введите 4-значное число с неповторяющимися цифрам,не начинающееся на 0: ')
+        if not bool(re.fullmatch(r"[1-9]\d\d\d", inp)) or len(set(inp)) != len(inp):
+            print('Ошибка ввода.')
+            continue
         inp = [int(d) for d in str(inp)]
         bull, cow = 0, 0
         for i, v in enumerate(inp):
-            if v == hidden_number[i]:
+            if v in hidden_number and v == hidden_number[i]:
                 bull += 1
-            if v in hidden_number:
+            if v in hidden_number and v != hidden_number[i]:
                 cow += 1
         if bull != 4:
             print(f'{cow} коровы, {bull} бык')
         else:
+            print('Вы выиграли!')
             break
-        inp = int(input())
-    print('Вы выиграли!')
 
 
 def pyramid(n: int) -> None:
@@ -31,10 +36,9 @@ def pyramid(n: int) -> None:
         print('*' * i)
 
 
-def statues(inp):
-    stat = set(map(int, inp.split()))
+def statues(inp: list) -> None:
+    if not all(isinstance(i, int) for i in inp):
+        raise ValueError("Все элементы списка должны быть числами.")
+    stat = set(inp)
     rng = set(range(min(stat), max(stat)))
-    print(rng - stat)
-
-
-statues('0 2 5 3 11')
+    print(len(rng - stat))
