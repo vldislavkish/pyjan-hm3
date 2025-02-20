d4bd67de9c88967ebb87b9246ccabdb5cd8ad8bd
uladek.kish <vldislav.kish@gmail.com>
2025-02-20 12:29:35 +0300

    hw#9

---
+++ hw#9.py
@@ -0,0 +1,27 @@
+def solution(sequence):
+    delete = 0
+    while len(sequence) >= 4:
+        if len(set(sequence[:3])) == 3 and min(sequence[:3]) == sequence[:3][0]:
+            del sequence[0]
+        elif len(set(sequence[:3])) == 2 and min(sequence[:3]) == sequence[:3][0] and delete == 0:
+            delete += 1
+            del sequence[0]
+        else:
+            return False
+    if len(set(sequence)) >= 1 and delete == 0 and min(sequence) == sequence[0]:
+        return True
+    else:
+        return False
+
+
+assert solution([1, 1]) == True
+assert solution([1]) == True
+assert solution([1, 2]) == True
+assert solution([1, 3, 2]) == True
+assert solution([1, 1, 2]) == True
+assert solution([1, 1, 1, 2]) == False
+assert solution([1, 2, 3]) == True
+assert solution([1, 2, 1, 2]) == False
+assert solution([1, 3, 2, 1]) == False
+assert solution([1, 2, 3, 4, 5, 3, 5, 6]) == False
+assert solution([40, 50, 60, 10, 20, 30]) == False
