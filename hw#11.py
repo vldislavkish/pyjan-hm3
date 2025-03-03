aaad77358f3195bf9440c237cd09823be783384e
uladek.kish <vldislav.kish@gmail.com>
2025-03-03 16:47:43 +0300

    hw#11

--- hw#11.py
+++ hw#11.py
@@ -20,11 +20,12 @@ print(example_function(-1, 2, 3))
 # Вернуть число
 def validate_result(func):
     def wrapper(*args):
-        if not all([isinstance(arg, (int, float)) for arg in args]):
+        if not all(isinstance(arg, (int, float)) for arg in args):
             print('Результат функции должен быть числом')
         else:
             print(f'{func(*args)} = {type(func(*args))}')
     return wrapper
+
 
 @validate_result
 def example_function1(a, b):
@@ -49,18 +50,23 @@ def typed(style):
 def add(a, b):
     return a + b
 
+
 add("3", 5)
 add(5, 5)
 add('a', 'b')
+
 
 @typed(style=int)
 def add2(a, b, c):
     return a + b + c
 
+
 add2(5, 6, 7)
+
 
 @typed(style=float)
 def add3(a, b, c):
     return a + b + c
+
 
 add3(0.1, 0.2, 0.4)
