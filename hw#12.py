d068edb7a5639337276d73ea6ad899e2d7db15ab
uladek.kish <vldislav.kish@gmail.com>
2025-03-06 14:44:24 +0300

    hw#12

--- hw#12.py
+++ hw#12.py
@@ -11,7 +11,6 @@ class Bank:
         print(f'Ваш ID: {client_id}')
         self._data.setdefault(client_id, {'name': name})
 
-
     def open_deposit_account(self):
         client_id = input('Введите Ваш ID: ')
         if client_id in self._data:
@@ -21,7 +20,6 @@ class Bank:
         else:
             print('Ошибка. Неправильно введён ID')
 
-
     def calc_deposit_interest_rate(self):
         client_id = input('Введите Ваш ID: ')
         if client_id in self._data and len(self._data.get(client_id)) > 1:
@@ -29,10 +27,10 @@ class Bank:
             balance = self._data.get(client_id).get('start_balance')
             procent = 10 / 12 / 100
             dep = tuple(balance + (balance * procent * (i + 1)) for i in range(month))
-            print(f'За {month} месяцев на Вашем балансе будет {dep[-1]} под 10% годовых, разница: {dep[-1] - balance}')
+            print(f'За {month} месяцев на Вашем балансе будет {dep[-1]} под 10% годовых,'
+                  f' разница: {dep[-1] - balance}')
         else:
             print('Ошибка. Неправильно введён ID или не открыт депозит')
-
 
     def close_deposit(self):
         client_id = input('Введите Ваш ID: ')
