7f8983a836d4fc90f09f239bd7097bc533deadc1
uladek.kish <vldislav.kish@gmail.com>
2025-03-10 15:53:40 +0300

    hw#13

--- hw#13.py
+++ hw#13.py
@@ -25,5 +25,30 @@ class Cards:
             print(dict(enumerate(self.shuffle_cards_54(), start=1))[num_card])
 
 
-card = Cards
-card()
+class CurrencyConverter:
+
+    _data = {'BYN': {'USD': 3.2125, 'EUR': 3.489, 'RUB': 0.0357},
+             'USD': {'BYN': 3.2125, 'EUR': 0.9208, 'RUB': 89.9155},
+             'EUR': {'USD': 1.0861, 'BYN': 3.489, 'RUB': 97.6545},
+             'RUB': {'USD': 0.0111, 'BYN': 0.0357, 'EUR': 0.0102}}
+
+    def __init__(self):
+        for i, v in enumerate(self._data, start=1):
+            print(i, v, sep=': ')
+
+        que1 = int(input('Выберите валюту, которую хотите конвертировать: '))
+        amount = float(input('Введите сумму выбранной валюты: '))
+        val1 = list(self._data.keys())[que1 - 1]
+
+        for i, v in enumerate(self._data[val1], start=1):
+            print(i, v, sep=': ')
+
+        que2 = int(input(f'Выберите заданную валюту: '))
+        val2 = list(self._data[val1].keys())[que2 - 1]
+
+
+        print(f'Из {amount} {val1} получится {round(amount / self._data[val1][val2], 3)} {val2}')
+
+
+cur = CurrencyConverter
+cur()
