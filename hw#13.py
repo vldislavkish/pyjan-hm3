cba27f94ffbae9caf5506cf8b5fe92cecf005e84
uladek.kish <vldislav.kish@gmail.com>
2025-03-10 13:16:54 +0300

    hw#13

--- hw#13.py
+++ hw#13.py
@@ -5,7 +5,7 @@ class Cards:
 
     @staticmethod
     def list_cards_54():
-        inds = ['A'] + [str(_) for _ in range(2, 11)] + [_ for _ in 'JQK']
+        inds = ['A'] + [str(_) for _ in range(2, 11)] + list('JQK')
         masts = 'Clubs ', 'Diamonds ', 'Hearts ', 'Spades '
         jokers = 'Red Joker', 'Black Joker'
         return [mast + ind for mast in masts for ind in inds] + list(jokers)
@@ -20,9 +20,10 @@ class Cards:
         flag = input('Хотите перемешать колоду? (+/-) ')
         num_card = int(input('Выберите карту из колоды в 54 карт: '))
         if flag == '-':
-            print({i: v for i, v in enumerate(self.list_cards_54(), start=1)}[num_card])
+            print(dict(enumerate(self.list_cards_54(), start=1))[num_card])
         elif flag == '+':
-            print({i: v for i, v in enumerate(self.shuffle_cards_54(), start=1)}[num_card])
+            print(dict(enumerate(self.shuffle_cards_54(), start=1))[num_card])
+
 
 card = Cards
 card()
