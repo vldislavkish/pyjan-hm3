170732af1eefec286f9fa7d57490c791b317b8c8
uladek.kish <vldislav.kish@gmail.com>
2025-03-11 18:04:04 +0300

    hw#14-15

--- hw#14-15.py
+++ hw#14-15.py
@@ -9,8 +9,9 @@ class Students:
         students = ['sasha', 'tanya', 'masha', 'kirill', 'sergei', 'mihail', 'gleb']
         file = open('students.txt', 'w', encoding='UTF-8')
         for _ in range(random.randint(3, 6)):
-            file.write(f'\t\tГруппа {''.join(random.choice(string.hexdigits) for _ in range(12))}\n')
-            file.write(f'Студент:\tОценка:\n')
+            file.write(f'\t\tГруппа {''.join(random.choice(string.hexdigits) 
+                                             for _ in range(12))}\n')
+            file.write('Студент:\tОценка:\n')
             for _ in range(random.randint(3, 7)):
                 file.write(f'\t{random.choice(students)}\t\t\t{random.randint(1, 10)}\n')
         file.close()
