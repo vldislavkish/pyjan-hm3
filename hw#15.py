d739459e8cf55655a7145c97ae6db91e423dae90
uladek.kish <vldislav.kish@gmail.com>
2025-03-16 20:19:00 +0300

    hw#14

--- hw#15.py
+++ hw#15.py
@@ -23,14 +23,17 @@ stream.setFormatter(formatter)
 logger.addHandler(stream)
 
 
+class OrderStatus(Enum):
+    PENDING = 1
+    IN_PROGRESS = 2
+    READY = 3
+    COMPLETED = 4
+    CANCELLED = 5
+
+
 class Order:
-    _OrderStatus = iter(Enum(
-        'OrderStatus', 'PENDING '
-                        'IN_PROGRESS '
-                        'READY '
-                        'COMPLETED '
-                        'CANCELLED ', start=1))
-    _data = {}
+    _OrderStatus = iter(OrderStatus)
+    _data: dict[str, OrderStatus] = {}
 
     def __init__(self, order_id: str):
         self.order_id = order_id
@@ -39,13 +42,20 @@ class Order:
     @classmethod
     def update_status(cls, order_id: str):
         if order_id in cls._data:
-            cls._data[order_id] = next(cls._OrderStatus)
+            try:
+                cls._data[order_id] = next(cls._OrderStatus)
+            except StopIteration:
+                # Если достигнут конец последовательности статусов
+                logger.warning('Статус для заказа %s уже на последнем этапе.', order_id)
         else:
-            logger.debug('Ошибка')
+            logger.debug('Ошибка: заказ не найден.')
 
     @classmethod
     def display_status(cls, order_id: str):
-        logger.info('Статус: %s', cls._data[order_id])
+        if order_id in cls._data:
+            logger.info('Статус заказа %s: %s', order_id, cls._data[order_id].name)
+        else:
+            logger.error('Заказ с ID %s не найден.', order_id)
 
 
 def day_difference(first_date: str, second_date: str):
@@ -75,10 +85,9 @@ def loger_actions():
 
         # Имитация ошибки
         raise ValueError("Ошибка чтения файла")
-    except Exception as e:
+    except ValueError as e:
         logger.error("Возникла ошибка: %s", e)
+    except FileNotFoundError as e:
+        logger.error("Файл не найден: %s", e)
     finally:
         logger.info("Завершение программы")
-
-
-loger_actions()
